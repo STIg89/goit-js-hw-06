3199a037ea309c21f7b3c4b296b9ab0b5ae5683c
STIg89 <andreystig89@gmail.com>
2022-10-28 22:46:44 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+
+  const email = formEl.email.value;
+  const password = formEl.password.value;
+
+  if (formEl.email.value && formEl.password.value !== '') {
+    const formData = { email, password };
+    console.log(formData);
+    form.reset();
+  } else {
+    window.alert('Все поля должны быть заполнены!');
+  }
+};
+
+form.addEventListener('submit', onFormSubmit);
