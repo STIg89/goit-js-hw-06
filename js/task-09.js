d6faf7aec39f430ad4260bdecc744a9706c54a8f
STIg89 <andreystig89@gmail.com>
2022-10-29 01:48:37 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const refs = {
+  body: document.querySelector('body'),
+  buttonChangeColor: document.querySelector('.change-color'),
+  colorValue: document.querySelector('.color'),
+};
+
+const bodyChangeColor = () => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.colorValue.textContent = `${getRandomHexColor()}`;
+};
+
+refs.buttonChangeColor.addEventListener('click', bodyChangeColor);
+
+const getRandomHexColor = () =>
+  `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
